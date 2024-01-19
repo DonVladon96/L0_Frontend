5acc4ce4de40ce8d5a458838ffc5720d81edcbdd
DonVladon96 <Petrov.Vladik2010@yandex.com>
2024-01-19 18:19:13 +0400

    update config, add img cards

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,66 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
   entry: {
-    index: path.resolve(__dirname, 'src', 'pages', 'index.js'),
+    main: path.resolve(__dirname, 'src', 'pages', 'index.js')
   },
-  devtool: 'inline-source-map',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080,
+    watchFiles: ['*/**/*.html']
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { importLoaders: 1 },
-          },
-        ],
-      },
       {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: '/node_modules/'
       },
-      // добавили правило для обработки файлов
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[ext]',
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
       },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src', 'index.html')
+      template: './src/index.html'
     }),
-    new MiniCssExtractPlugin()
-  ],
-  devServer: {
-    port: 8080,
-    open: true,
-    hot: true,
-    watchFiles: ['*/**/*.html']
-  }
-};
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
+}
